8fdefb18c656a5417b39c8faf1077dc405c92a14
Alyssa Cabading <alyssacabading@yahoo.com>
2024-10-22 15:42:43 -0500

    Contacts CRUD

---
+++ src/routes/contactRoutes.js
@@ -0,0 +1,60 @@
+import { Router } from 'express';
+import { ContactService } from '../services/contactService.js';
+
+const contactService = new ContactService();
+const router = Router();
+
+// Create Contact
+router.post('/', async (req, res) => {
+    try {
+        const data = req.body;
+        const newCon = await contactService.createContact(data);
+        res.status(201).json(newCon);
+    } catch (error) {
+        res.status(500).json({ error: 'Error creating contact.', details: error });
+    }
+});
+
+// Get Contact by Company Name
+router.get('/:company', async (req, res) => {
+    try {
+        const name = req.params.company;
+        const contact = await contactService.getContactbyCompany(name);
+        if (!contact) {
+            return res.status(404).json({ error: 'Contact not found.'})
+        };
+        res.status(200).json(contact);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching contacts', details: error })
+    }
+})
+
+// Update Contact By ID
+router.put('/:id', async (req, res) => {
+    try {
+        const conId = req.params.id;
+        const contact = await contactService.updateContact(conId, req.body);
+        if (!contact) {
+          return res.status(404).json({ error: 'Contact not found' });
+        }
+        res.status(200).json(contact);
+    } catch (error) {
+        res.status(500).json({ error: 'Error updating contact', details: error });
+    }
+});
+
+// Delete Contact By ID
+router.delete('/:id', async (req, res) => {
+    try {
+        const conId = req.params.id;
+        const contact = await contactService.deleteContact(conId);
+        if (!contact) {
+          return res.status(404).json({ error: 'Contact not found' });
+        }
+        res.status(204).send();
+    } catch (error) {
+        res.status(500).json({ error: 'Error deleting contact', details: error });
+    }
+});
+
+export default router;
---
+++ src/services/contactService.ts
@@ -0,0 +1,22 @@
+import Contact, { IContact } from "../models/contact.js";
+
+export class ContactService {
+    async createContact (contactData: IContact): Promise<IContact> {
+        const contact = new Contact(contactData);
+        return await contact.save();
+    }
+
+    async getContactbyCompany (companyName: string): Promise<IContact[]> {
+        return await Contact.find({
+            company: companyName
+        });
+    }
+
+    async updateContact (contactId: string, contactData: Partial<IContact>): Promise<IContact | null> {
+        return await Contact.findByIdAndUpdate(contactId, contactData, { new: true });
+    }
+
+    async deleteContact(contactId:string): Promise<IContact | null> {
+        return await Contact.findByIdAndDelete(contactId);
+    }
+}
