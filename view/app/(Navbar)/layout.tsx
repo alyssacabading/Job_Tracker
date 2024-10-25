888907c5c5560316ab910028e9043a21c92334f1
MattnViola <mrnorman.sev@gmail.com>
2024-10-24 21:06:58 -0400

    Adds a simple accordion to Applications

--- view/app/(Navbar)/layout.tsx
+++ view/app/(Navbar)/layout.tsx
@@ -1,7 +1,11 @@
 import "../globals.css";
 import Navbar from "../components/Navbar";
 
-export default function NavbarLayout({ children }: { children: React.ReactNode }) {
+export default function NavbarLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
