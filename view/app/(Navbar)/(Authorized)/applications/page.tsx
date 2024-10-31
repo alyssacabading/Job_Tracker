64a6776575d6eb3ebfa51be9d3a69427895e3498
Aline Murillo <alinemurillo33@gmail.com>
2024-10-30 19:38:52 -0700

    Initial commit for add application modal

--- view/app/(Navbar)/(Authorized)/applications/page.tsx
+++ view/app/(Navbar)/(Authorized)/applications/page.tsx
@@ -2,23 +2,44 @@
 
 import { useState } from "react";
 import { FaChevronDown, FaChevronUp } from "react-icons/fa"; // Chevron icons
+import AddApplicationModal from "./AddApplicationModal";
 
 export default function Applications() {
   const [isOpen, setIsOpen] = useState(false);
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
 
   // Toggle the accordion open/close
   const toggleAccordion = () => {
     setIsOpen(!isOpen);
   };
 
+  // Open the modal
+  const openModal = () => {
+    setIsAddModalOpen(true);
+  };
+
+  // Close the modal
+  const closeModal = () => {
+    setIsAddModalOpen(false);
+  };
+
   return (
-    <div className="flex justify-center pt-10">
+    <div className="max-w-screen-md mx-auto flex flex-col items-center pt-10">
+      <div className="flex justify-end w-full mb-4">
+        <button
+          onClick={openModal}
+          className="bg-customblue hover:bg-custombluehover text-white p-2.5 rounded-lg mb-4 font-bold transition ease-in-out"
+          >
+            Add Application
+        </button>
+      </div>
+
       <div
-        className="w-1/2 bg-white rounded-lg shadow-2xl p-4 cursor-pointer"
+        className="w-full bg-white rounded-lg shadow-2xl p-4 cursor-pointer"
         onClick={toggleAccordion}
       >
         <div className="flex justify-between items-center">
-          <h2 className="text-customdarkgrey text-xl">Notion Internship</h2>
+          <h2 className="text-customdarkgrey text-xl font-bold">Notion Internship</h2>
           <div className="text-customdarkgrey">
             {isOpen ? <FaChevronUp /> : <FaChevronDown />}
           </div>
@@ -38,6 +59,8 @@ export default function Applications() {
           </div>
         )}
       </div>
+
+      <AddApplicationModal isOpen={isAddModalOpen} onClose={closeModal} />
     </div>
   );
 }
