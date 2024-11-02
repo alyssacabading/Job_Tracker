c6872c069102083c8f8ed12f9db9b336df6044b0
ahleeneh <107948221+ahleeneh@users.noreply.github.com>
2024-11-01 17:07:31 -0700

    Add modal to edit application and confirm deletion (#14)

--- view/app/(Navbar)/(Authorized)/applications/page.tsx
+++ view/app/(Navbar)/(Authorized)/applications/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
 import { useState } from "react";
-import { FaChevronDown, FaChevronUp } from "react-icons/fa"; // Chevron icons
+import { FaChevronDown, FaChevronUp, FaEdit } from "react-icons/fa";
 import AddApplicationModal from "./AddApplicationModal";
+import EditApplicationModal from "./EditApplicationModal";
 
 // TODO: Update the Application interface to align with the backend
 export interface Application {
@@ -53,7 +54,9 @@ const defaultApplications: Application[] = [
 export default function Applications() {
   const [openAccordions, setOpenAccordions] = useState<number[]>([]);
   const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
   const [apps, setApps] = useState(defaultApplications);
+  const [selectedApp, setSelectedApp] = useState<Application | null>(null);
 
   const toggleAccordion = (id: number) => {
     setOpenAccordions((prev) =>
@@ -61,16 +64,17 @@ export default function Applications() {
     );
   };
 
-  // Open the modal
+  // Open the add modal
   const openModal = () => {
     setIsAddModalOpen(true);
   };
 
-  // Close the modal
+  // Close the add modal
   const closeModal = () => {
     setIsAddModalOpen(false);
   };
 
+  // Add a new application
   const addApplication = (application: Application) => {
     setApps((prev) => [
       ...prev,
@@ -78,8 +82,33 @@ export default function Applications() {
     ]);
   };
 
+  // Open the edit modal
+  const openEditModal = (application: Application) => {
+    setSelectedApp(application);
+    setIsEditModalOpen(true);
+  };
+
+  // Close the edit modal
+  const closeEditModal = () => {
+    setSelectedApp(null);
+    setIsEditModalOpen(false);
+  };
+
+  // Edit an existing application
+  const editApplication = (application: Application) => {
+    setApps((prev) => prev.map((app) => (app.id === application.id ? application : app)));
+    closeEditModal();
+  };
+
+  // Delete an existing application
+  const deleteApplication = (id: number) => {
+    setApps((prev) => prev.filter((app) => app.id !== id));
+    closeEditModal();
+  };
+
   return (
     <div className="max-w-screen-md mx-auto flex flex-col items-center pt-10">
+      {/* Add a button to open the AddApplicationModal */}
       <div className="flex justify-end w-full mb-4">
         <button
           onClick={openModal}
@@ -89,19 +118,31 @@ export default function Applications() {
         </button>
       </div>
 
+      {/* Display all applications */}
       {apps.map((application) => (
         <div
           key={application.id}
           className="w-full bg-white rounded-lg shadow-xl p-4 cursor-pointer mb-5"
           onClick={() => toggleAccordion(application.id)}
         >
+          {/* Display the application title and company */}
           <div className="flex justify-between items-center">
             <h2 className="text-customdarkgrey text-xl font-bold">{application.title} @ {application.company}</h2>
-            <div className="text-customdarkgrey">
+            <div className="text-customdarkgrey flex flex-row ">
+              {/* Add an edit button to open the EditApplicationModal */}
+              <FaEdit
+                onClick={(e) => {
+                  e.stopPropagation();
+                  openEditModal(application);
+                }}
+                className="text-xl cursor-pointer hover:text-black transition ease-in-out mr-3"
+              />
+              {/* Add a chevron icon to indicate the accordion state */}
               {openAccordions.includes(application.id) ? <FaChevronUp /> : <FaChevronDown />}
             </div>
           </div>
 
+          {/* Display the application details */}
           {openAccordions.includes(application.id) && (
             <div className="text-left text-customdarkgrey mt-4">
               <p className="text-lg mb-2">
@@ -118,7 +159,17 @@ export default function Applications() {
         </div>
       ))}
 
+      {/* Add the AddApplicationModal and EditApplicationModal components */}
       <AddApplicationModal isOpen={isAddModalOpen} onClose={closeModal} addApplication={addApplication} />
+      {selectedApp && (
+        <EditApplicationModal 
+          isOpen={isEditModalOpen}
+          onClose={closeEditModal}
+          editApplication={editApplication}
+          deleteApplication={deleteApplication}
+          application={selectedApp}
+        />
+      )}
     </div>
   );
 }
