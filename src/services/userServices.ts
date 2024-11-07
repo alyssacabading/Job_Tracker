e91d3c248cf10bfa7d27e4fb0be7277459e10075
Alyssa Cabading <alyssacabading@yahoo.com>
2024-11-07 13:56:07 -0600

    Typo fix'

--- src/services/userServices.ts
+++ src/services/userServices.ts
@@ -1,4 +1,4 @@
-import User, { IUser } from '../models/User.js';
+import User, { IUser } from '../models/user.js';
 
 export class UserService {
     async createUser (userData: IUser): Promise<IUser> {
