2d1120400b2f1c3b98f9874024ed5eba636fcc1b
erik blackowicz <blackowe@oregonstate.edu>
2024-11-20 12:58:42 -0600

    Implemented 'validateId' fx into service layer. Added null check for 'phone' property

--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -1,7 +1,12 @@
 import Contact, { IContact } from "../models/contact.js";
+import { EntityType, validateId } from "../utility/idValidation.js";
 
 export class ContactService {
     async createContact (contactData: IContact): Promise<IContact> {
+        // set 'phone' to null if not provided
+        if (contactData.phone === undefined) {
+            contactData.phone = null;
+        }
         const contact = new Contact(contactData);
         return await contact.save();
     }
@@ -11,16 +16,20 @@ export class ContactService {
     }
 
     async getContactbyCompany (companyName: string): Promise<IContact[]> {
-        return await Contact.find({
-            company: companyName
-        });
+        const contacts = await Contact.find({ company: companyName });
+        if (contacts.length === 0) {
+            throw new Error('Contact not found in the database');
+        }
+        return contacts;
     }
 
     async updateContact (contactId: string, contactData: Partial<IContact>): Promise<IContact | null> {
+        await validateId(contactId, EntityType.Contact);
         return await Contact.findByIdAndUpdate(contactId, contactData, { new: true });
     }
 
     async deleteContact(contactId:string): Promise<IContact | null> {
+        await validateId(contactId, EntityType.Contact);
         return await Contact.findByIdAndDelete(contactId);
     }
 }
